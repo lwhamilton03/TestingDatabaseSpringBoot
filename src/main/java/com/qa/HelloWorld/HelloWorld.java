3bb01de725bbece9f66dc281f239a6b02a14048c
lwhamliton03 <lucy.hamilton@academytrainee.comgit config --global user.email lucy.hamilton@academytrainee.comgit config --global user.email lucy.hamilton@academytrainee.com>
2018-10-08 08:17:09 +0100

    testing altered

--- src/main/java/com/qa/HelloWorld/HelloWorld.java
+++ src/main/java/com/qa/HelloWorld/HelloWorld.java
@@ -1,13 +1,15 @@
 //package com.qa.HelloWorld;
 //
 //
+//import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 //
+//@RequestMapping("/")
 //@RestController
 //public class HelloWorld {
 //
-//	@RequestMapping("/")
+//	@GetMapping("/")
 //	public String helloWorld()
 //	{
 //		return "Hello World!";
--- src/main/java/com/qa/mySpringBootDatabaseApp/controller/PersonController.java
+++ src/main/java/com/qa/mySpringBootDatabaseApp/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,15 @@ public class PersonController
 	
 	@Autowired
 	private MySpringBootRepository mySpringBootRepository; 
+	
+	@PostMapping("/person/{personId}/orders")
+	public Order createComment(@PathVariable (value = "personId") Long personId, @Valid @RequestBody Order order)
+	{
+		return mySpringBootRepository.findById(personId)
+				.map(mySpringBootDataModel -> {order.setPerson(mySpringBootDataModel);
+		return orderRepository.save(order);})
+				.orElseThrow(() -> new ResourceNotFoundException("Person", "id", order));
+	}
 	
 	@GetMapping("/person/{personId}/orders")
 	public Page<Order> getAllOrdersByPersonId(@PathVariable(value = "person_id") Long personId, Pageable pageable)
